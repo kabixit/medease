890214523aff7bf705410107a7ece2e9520b5f71
Kabilan <kumarakabilan2004@gmail.com>
2024-03-14 10:52:49 +0530

    Pushhh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, signOut as firebaseSignOut } from 'firebase/auth';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+import { app } from './FirebaseProvider';
+import SignupPage from './RegisterPage';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(getAuth(app), provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const signInWithFacebook = async () => {
+    const provider = new FacebookAuthProvider();
+    try {
+      const result = await signInWithPopup(getAuth(app), provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const signOut = () => {
+    firebaseSignOut(getAuth(app)).then(() => {
+      setUser(null);
+    }).catch((error) => {
+      console.error(error);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={user ? <HomePage user={user} signOut={signOut} /> : <Navigate to="/" />} />
+        <Route path="/" element={<LoginPage auth={getAuth(app)} signInWithGoogle={signInWithGoogle} signInWithFacebook={signInWithFacebook} />} />
+        <Route path="signup" element={<SignupPage auth={getAuth(app)} signInWithGoogle={signInWithGoogle} signInWithFacebook={signInWithFacebook} />} />
+      </Routes>
+    </Router>
   );
 }
 
